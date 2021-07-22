d3ca63e7a33f04b7a6b64b499ca3f863893a2171
Sebastian Kotowski <kotowskis@gmail.com>
2021-07-22 13:48:49 +0200

    flyout page

--- CoursesMobile/App.xaml.cs
+++ CoursesMobile/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoursesMobile.Helpers;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace CoursesMobile
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new NavigationPageView());
+            MainPage = new FlyoutPageView2();
         }
 
         protected override void OnStart()
---
+++ CoursesMobile/FlyoutPageView2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoursesMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyoutPageView2 : FlyoutPage
+    {
+        public FlyoutPageView2()
+        {
+            InitializeComponent();
+
+            masterView.NavigationListView.ItemSelected += NavigationListView_ItemSelected;
+        }
+
+        private void NavigationListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem is MasterNavigationItem item)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.Target));
+                masterView.NavigationListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
